17d7c0b502c51d575037fd02f019ddaff9c489fb
Ryochi <ryo1.t.zubu@gmail.com>
2021-12-20 22:27:08 +1000

    done

--- js/reducer.js
+++ js/reducer.js
@@ -1,13 +1,23 @@
+let state = {count: 0};
+
 function changeState(state, action){
-  switch (action.type) {
-    case 'INCREASE_COUNT':
+  switch(action.type){
+    case "INCREASE_COUNT":
       return {count: state.count + 1}
-    default:
-      return state;
+      default:
+        return state;
   }
 }
 
-let state = {count: 0}
-let action = {type: 'INCREASE_COUNT'}
+function dispatch(action){
+  state = changeState(state, action)
+  return state;
+}
 
-changeState(state, action)
+function render(){
+  document.body.textContent = state.count
+}
+
+render();
+
+dispatch({type: 'INCREASE_COUNT'});
